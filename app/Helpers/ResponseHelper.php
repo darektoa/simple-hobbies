ce3098f1f997ca3942a329ee9e6308a6503dcb95
darektoa <darektoa@gmail.com>
2022-07-05 09:57:22 +0700

    Add: Make static method in response helper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -3,5 +3,11 @@
 namespace App\Helpers;
 
 class ResponseHelper {
-    
+    static public function make($data=[], string $message='OK', $status=200) {
+        return response()->json([
+            'status'  => $status,
+            'message' => $message,
+            'data'    => $data,
+        ], $status);
+    }
 }
