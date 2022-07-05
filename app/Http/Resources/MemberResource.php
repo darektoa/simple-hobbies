9b8f844499790eb68d0001f5e562c0db30e7ecd0
darektoa <darektoa@gmail.com>
2022-07-05 14:42:22 +0700

    Add: Prperties in member resource

--- app/Http/Resources/MemberResource.php
+++ app/Http/Resources/MemberResource.php
@@ -14,6 +14,13 @@ class MemberResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'            => $this->id,
+            'name'          => $this->name,
+            'email'         => $this->email,
+            'phone'         => $this->phone,
+            'created_at'    => $this->created_at,
+            'hobbies'       => HobbyResource::collection($this->whenLoaded('hobbies')),
+        ];
     }
 }
