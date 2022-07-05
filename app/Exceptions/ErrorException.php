031d6ea0fa4e273425ac7883b817f91ae7a1765c
darektoa <darektoa@gmail.com>
2022-07-05 09:59:25 +0700

    Add: Overide exception consturctor in custom error exception

--- app/Exceptions/ErrorException.php
+++ app/Exceptions/ErrorException.php
@@ -3,8 +3,19 @@
 namespace App\Exceptions;
 
 use Exception;
+use Throwable;
 
 class ErrorException extends Exception
 {
+    private $errors;
 
+    public function __construct(
+        string $message = '',
+        int $code = 400, 
+        $errors = [],
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->errors = $errors;
+    }
 }
