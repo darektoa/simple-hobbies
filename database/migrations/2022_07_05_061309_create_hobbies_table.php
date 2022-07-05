c14134fd6f47f5b2c17bfae6aff611f74e77082f
darektoa <darektoa@gmail.com>
2022-07-05 14:10:09 +0700

    Edit: User id field to member id field in hobbies table migration

--- database/migrations/2022_07_05_061309_create_hobbies_table.php
+++ database/migrations/2022_07_05_061309_create_hobbies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('hobbies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('member_id')->constrained();
             $table->string('name', 100);
             $table->timestamps();
         });
