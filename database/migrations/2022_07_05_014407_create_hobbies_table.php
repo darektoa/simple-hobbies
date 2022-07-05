d180ea60bbe27a6dc92f1be2477c90c4e7342046
darektoa <darektoa@gmail.com>
2022-07-05 08:49:13 +0700

    Add: Fields in hobbies table migration

--- database/migrations/2022_07_05_014407_create_hobbies_table.php
+++ database/migrations/2022_07_05_014407_create_hobbies_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('hobbies', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
