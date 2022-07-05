31333605a9d3b4c75cd8ab72eaf0f95d97cb381d
darektoa <darektoa@gmail.com>
2022-07-05 12:57:33 +0700

    Add: Hobbies relation method in member model

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -15,4 +15,9 @@ class Member extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+
+    public function hobbies() {
+        return $this->hasMany(Hobby::class);
+    }
 }
