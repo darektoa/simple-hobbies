aea34850e54fcd788a036d3337667051579c853c
darektoa <darektoa@gmail.com>
2022-07-05 09:57:55 +0700

    Add: Paginate static method in response helper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+
 class ResponseHelper {
     static public function make($data=[], string $message='OK', $status=200) {
         return response()->json([
@@ -18,5 +20,24 @@ class ResponseHelper {
             'message'   => $message,
             'errors'    => $errors,
         ], $status);
+    }
+
+
+    static public function paginate($data=[], string $message='OK', $status=200) {
+        $response = collect([
+            'status'    => $status,
+            'message'   => $message,
+        ]);
+
+        if($data instanceof JsonResource) $response = $response->merge([
+            'data'          => $data,
+            'current_page'  => $data->resource->currentPage(),
+            'last_page'     => $data->resource->lastPage(),
+            'per_page'      => $data->resource->perPage(),
+            'total'         => $data->resource->total(),
+        ]);
+        else $response = $response ->merge($data);
+
+        return response()->json($response);
     }
 }
