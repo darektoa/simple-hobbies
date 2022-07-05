98e4268ea5be3c938082771e7df9249a274c6054
darektoa <darektoa@gmail.com>
2022-07-05 10:05:00 +0700

    Add: Validate method in API request validator trait

--- app/Traits/Api/RequestValidator.php
+++ app/Traits/Api/RequestValidator.php
@@ -2,6 +2,29 @@
 
 namespace App\Traits\Api;
 
-trait RequestValidator{
+use App\Exceptions\ErrorException as Error;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
+trait RequestValidator{
+    public function validate(
+        Request $request,
+        array $rules,
+        array $message=[],
+        array $customAttribute=[]
+    ) {
+        $validator = Validator::make(
+            $request->all(),
+            $rules,
+            $message,
+            $customAttribute,
+        );
+
+        if($validator->fails()) {
+            $errors = $validator->errors()->all();
+            return throw new Error('Unprocessable', 422, $errors);
+        }
+
+        return $validator;
+    }
 }
