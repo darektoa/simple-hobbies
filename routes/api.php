ccac35db1acbad086833d655e27a204d6562939e
darektoa <darektoa@gmail.com>
2022-07-05 11:30:38 +0700

    Add: Auth signup route with post method in API routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
+Route::prefix('/auth')->group(function() {
+    Route::post('/signup', [AuthController::class, 'signUp']);
+});
