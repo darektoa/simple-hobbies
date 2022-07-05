eb7cdcbb932281255cd2f2b858ac7a8b09c1f673
darektoa <darektoa@gmail.com>
2022-07-05 12:06:40 +0700

    Add: Signout route with post method in API routes

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,14 @@ Route::prefix('/auth')->group(function() {
     Route::post('/signin', [AuthController::class, 'signIn']);
     Route::post('/signup', [AuthController::class, 'signUp']);
 });
+
+
+/*
+    WITH AUTHENTICATION
+*/
+Route::middleware('auth:api')->group(function() {
+    // AUTH
+    Route::prefix('/auth')->group(function() {
+        Route::post('/signout', [AuthController::class, 'signOut']);
+    });
+});
